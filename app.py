f0f4a879eb7b62be6052f9b5bcfa2aa9c23b11c2
Arnau López <arnaul445@gmail.com>
2026-01-29 22:46:59 +0100

    arreglo landing politiques RGPD

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/cookies')
+def cookies():
+    return render_template('cookies.html')
+
+@app.route('/privacidad')
+def privacidad():
+    return render_template('privacidad.html')
+
+@app.route('/aviso-legal')
+def aviso_legal():
+    return render_template('aviso-legal.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
